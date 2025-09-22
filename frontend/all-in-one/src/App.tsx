7d196be60612c67cf3cffb1a8cc7677849cce31a
erjia77 <erjia.wang.2022@scis.smu.edu.sg>
2025-09-22 16:06:39 +0800

    frontend update for task management

--- frontend/all-in-one/src/App.tsx
+++ frontend/all-in-one/src/App.tsx
@@ -1,19 +1,20 @@
 import { Routes, Route, Link } from "react-router-dom";
-import MyTasks from "./pages/MyTasks.tsx";
+import TaskManager from "./components/TaskManager";
 import Auth from "./pages/Auth.tsx";
+import "./styles/Task.css"; // Import the new task styles
 
 function App() {
   return (
     <div>
       {/* navbar */}
       <nav>
-        <Link to="/">MyTasks</Link> |{" "}
+        <Link to="/">Task Manager</Link> |{" "}
         <Link to="/auth">Auth</Link> {" "}
       </nav>
 
       {/* routing */}
       <Routes>
-        <Route path="/" element={<MyTasks />} />
+        <Route path="/" element={<TaskManager />} />
         <Route path="/auth" element={<Auth />} />
       </Routes>
     </div>
