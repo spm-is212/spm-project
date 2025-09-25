193a1f9373ceabf8017b095da4639842b51d246a
Fong Shi Hui <shihui.fong.2023@smu.edu.sg>
2025-09-25 09:47:15 +0800

    fe and be integration + seed script (#14)
    * autogen uuid and update requirements.txt
    
    * add test_login.py and remove register
    
    * fix import
    
    * remove unused deps
    
    * Revert "fix import"
    
    This reverts commit c5757e765adc706c923cfbf0c0eb209c8ff46aa5.
    
    * Reapply "fix import"
    
    This reverts commit fb27d49601196c3551f1eec9b23e63ab8946b756.
    
    * fix
    
    * connect login backend to frontend and seed script for users table
    
    * remove extra import

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware 
 from backend.routers import auth, task, health, crud_test
 
 app = FastAPI(title="SPM Project API")
+
+origins = [
+    "http://localhost:5173",  
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,         
+    allow_credentials=True,
+    allow_methods=["*"],            # allow all HTTP methods
+    allow_headers=["*"],            # allow all headers
+)
 
 app.include_router(health.router)
 app.include_router(auth.router)
